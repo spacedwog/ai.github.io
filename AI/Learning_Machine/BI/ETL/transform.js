3957ba2399f845b9440a3fef54e2ff79cdfb4c9b
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-06 16:51:39 -0300

    Commited

--- AI/Learning_Machine/BI/ETL/transform.js
+++ AI/Learning_Machine/BI/ETL/transform.js
@@ -1,6 +1,6 @@
 function find_array(eixoX, eixoY, predictedX, predictedY) {
     let first = eixoX.find(find_array);
-    let second = eixoY.find(find_array)
+    let second = eixoY.find(find_array);
 }
 
 function find_array(eixoX, eixoY, predictedX, predictedY, indexX, indexY, array) {
