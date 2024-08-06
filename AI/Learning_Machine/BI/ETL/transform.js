4f55e062e974f2728a88a36b9a03e3fce5482c76
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-06 18:41:24 -0300

    Commited

--- AI/Learning_Machine/BI/ETL/transform.js
+++ AI/Learning_Machine/BI/ETL/transform.js
@@ -1,8 +1,25 @@
+const eixoX;
+const eixoY;
+
 function find_array(eixoX, eixoY, predictedX, predictedY) {
     let first = eixoX.find(find_array);
     let second = eixoY.find(find_array);
 }
 
-function find_array(value, index, array) {
+function find_Xarray(value, index, array) {
+    setEixoX(value);
+}
 
+function find_Yarray(value, index, array) {
+    setEixoY(value);
+}
+
+
+//Encapsulamento
+function setEixoX(value) {
+    eixoX = value;
+}
+
+function setEixoY(value) {
+    eixoY = value;
 }
