9c8fea9d41e7d2c79a4599751006fd8c3b0bc5f5
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-07 15:59:04 -0300

    Commited

--- AI/Learning_Machine/BI/ETL/transform.js
+++ AI/Learning_Machine/BI/ETL/transform.js
@@ -1,15 +1,40 @@
 function transformData(xArray, yArray) {
 
+    // Calculate Sums
+    let xSum = 0,
+        ySum = 0,
+        xxSum = 0,
+        xySum = 0;
+    let count = xArray.length;
+    for (let i = 0, len = count; i < count; i++) {
+        xSum += xArray[i];
+        ySum += yArray[i];
+        xxSum += xArray[i] * xArray[i];
+        xySum += xArray[i] * yArray[i];
+    }
+
+    // Calculate slope and intercept
+    let slope = (count * xySum - xSum * ySum) / (count * xxSum - xSum * xSum);
+    let intercept = (ySum / count) - (slope * xSum) / count;
+
+    // Generate values
+    const xValues = [];
+    const yValues = [];
+    for (let x = 50; x <= 150; x += 1) {
+        xValues.push(x);
+        yValues.push(x * slope + intercept);
+    }
+
     const data = [
         { x: xArray, y: yArray, mode: "markers" },
-        { x: [40, 240], y: [9, 50], mode: "line" },
+        { x: xValues, y: yValues, mode: "line" }
     ];
 
     const layout = {
-        xaxis: { range: [0, 240], title: "Horsepower" },
-        yaxis: { range: [0, 50], title: "MPG" },
+        xaxis: { range: [40, 160], title: "Square Meters" },
+        yaxis: { range: [5, 16], title: "Price in Millions" },
         title: "House Prices vs. Size"
     };
 
-    Plotly.newPlot("plot2", data, layout);
+    Plotly.newPlot("Plot2", data, layout);
 }
