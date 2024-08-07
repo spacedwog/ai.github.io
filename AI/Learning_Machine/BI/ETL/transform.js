82a415558a0873004619227da970f4a05829eb7a
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-07 18:16:08 -0300

    Commited

--- AI/Learning_Machine/BI/ETL/transform.js
+++ AI/Learning_Machine/BI/ETL/transform.js
@@ -17,7 +17,7 @@ function transformData(xArray, yArray) {
     let slope = (count * xySum - xSum * ySum) / (count * xxSum - xSum * xSum);
     let intercept = (ySum / count) - (slope * xSum) / count;
 
-    alert("Intercept: " + intercept.max())
+    alert("Intercept: " + intercept.max());
 
     // Generate values
     const xValues = [];
