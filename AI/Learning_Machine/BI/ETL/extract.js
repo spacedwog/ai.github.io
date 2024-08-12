f6d39c5ec1fb92e0fb8f5f24124033506c60dfaa
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-12 18:06:18 -0300

    Commited

--- AI/Learning_Machine/BI/ETL/extract.js
+++ AI/Learning_Machine/BI/ETL/extract.js
@@ -1,13 +1,10 @@
 // Extract Correct Data 
 function extractData(obj) {
-    alert(obj.Name);
     return {
         Name: obj.Name,
         x: obj.Horsepower,
         y: obj.Miles_per_Gallon
     };
-    localStorage.setItem("cilindros", obj.Cylinders);
-    localStorage.setItem("deslocamento", obj.Cylinders);
 }
 
 function removeErrors(obj) {
