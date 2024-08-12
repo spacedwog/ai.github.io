4a1e1c39aad97412798d0598bc8a151ea1410850
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-12 17:33:57 -0300

    Commited

--- AI/Learning_Machine/BI/ETL/extract.js
+++ AI/Learning_Machine/BI/ETL/extract.js
@@ -3,7 +3,13 @@ function extractData(obj) {
     return {
         Name: obj.Name,
         x: obj.Horsepower,
-        y: obj.Miles_per_Gallon
+        y: obj.Miles_per_Gallon,
+        cilindros: obj.Cylinders,
+        deslocamento: obj.Displacement,
+        peso_libras: obj.Weight_in_lbs,
+        aceleracao: obj.Acceleration,
+        ano_fabricacao: obj.Year,
+        origem_fabrica: obj.Origin
     };
 }
 
