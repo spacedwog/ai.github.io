f93188cb7502dcc66839042918c20a298bfaeb4b
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-12 19:20:20 -0300

    Commited

---
+++ AI/laplace_demon.js
@@ -0,0 +1,41 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    if (latitude == getLatitude() && longitude == getLongitude()) {
+        alert("Você está parado");
+    } else if (latitude > getLatitude() && longitude > getLongitude()) {
+        alert("Você andou para frente");
+    } else if (latitude < getLatitude() && longitude < getLongitude()) {
+        alert("Você andou para trás");
+    }
+}
+
+//Encapsulamento
+function setLatitude(latitude) {
+    localStorage.setItem("latitude", latitude);
+}
+
+function setLongitude(longitude) {
+    localStorage.setItem("longitude", longitude);
+}
+
+function getLatitude() {
+    const latitude = localStorage.getItem("latitude");
+
+    return latitude;
+}
+
+function getLongitude() {
+    const longitude = localStorage.getItem("longitude");
+
+    return longitude;
+}
