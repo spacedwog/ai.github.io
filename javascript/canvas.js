308e4cd69e06b8d32d3951d830366e364db14f20
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-18 17:22:38 -0300

    Commited

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,7 +1,7 @@
 function canvas_map() {
     const canvasmap = document.getElementById('canvas_screen');
 
-    var ctx = canvas_map.getContext("2d");
+    const ctx = canvasmap.getContext("2d");
     ctx.moveTo(0, 0);
     ctx.lineTo(500, 400);
     ctx.stroke();
