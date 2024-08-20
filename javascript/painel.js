f465e439e5dee3b9475f49b7aad763f9700b5a26
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-20 14:50:31 -0300

    Commited

--- javascript/painel.js
+++
@@ -1,8 +0,0 @@
-function canvas_painel() {
-    const c = document.getElementById("painel_administrativo");
-    const ctx = c.getContext("2d");
-
-    ctx.beginPath();
-    ctx.rect(20, 20, 150, 100);
-    ctx.stroke();
-}
