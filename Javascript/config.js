5c8983a53125b7147b4ac6d1113abb4e641d11b4
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-22 19:34:37 -0300

    Commited

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,16 +1,3 @@
 function config() {
-    const http = require("node:http");
     var myWindow = window.open("Configurações", "", "width=200,height=100");
-
-    const api = {
-        "host": "api.ipify.org",
-        "port": 80,
-        "path": "/"
-    };
-
-    http.get(api, response => {
-        response.on("data", ip => {
-            console.log('\nYour public IP address is: \x1b[33${ip}\x1b[0m\n');
-        });
-    });
 }
