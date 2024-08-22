a102bd40e62a38dec4a06c98d09e4acc63b61ce4
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-08-22 16:19:45 -0300

    Commited

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,3 +1,7 @@
-function config() {
-
-}
+$(document).ready(function() {
+    $("a").click(function() {
+        $.get("index.html", function(data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+});
