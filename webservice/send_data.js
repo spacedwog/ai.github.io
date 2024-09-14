9f549f871604d2811a7920dea05e7aaf2b257e4b
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-09-13 23:15:09 -0300

    Commited

--- webservice/send_data.js
+++ webservice/send_data.js
@@ -17,7 +17,7 @@ async function usuario() {
 }
 
 async function send_usuarioData(ipAddress) {
-    const response = await fetch('https://dummyjson.com/products/add', {
+    const response = await fetch('https://ai.spacedwog.com.br/usuario.json', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset=utf-8'
