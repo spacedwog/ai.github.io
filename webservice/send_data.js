de2217ed978d58f853beabf9233d0da6b793ffd7
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-09-16 18:28:33 -0300

    Commited

--- webservice/send_data.js
+++ webservice/send_data.js
@@ -1,11 +1,13 @@
 async function usuario() {
     let ipAddress;
+    const ipaddresshtml = document.getElementById('ipaddress').innerHTML;
     fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
         .then(data => {
             ipAddress = data.ip;
             setIpAddress(ipAddress);
             alert("Novo Usuário: " + getIpAddress());
+            ipAddress += "<p id='" + ipAddress + "'>" + "</p>";
             console.log(data.ip);
         })
         .catch(error => {
