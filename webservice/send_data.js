387f15cc7d4eee77ad9718d5127e2dc4b0bc2b8a
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-03 17:11:38 +0000

    Commited

--- webservice/send_data.js
+++ webservice/send_data.js
@@ -12,7 +12,7 @@ async function usuario() {
             const xmlhttp = new XMLHttpRequest();
             alert("Requesito html");
             xmlhttp.onload = function() {
-                alert("xmlhttp: " + myObj);
+                alert("xmlhttp: " + getIpAddress());
                 const myObj = JSON.parse(getIpAddress());
                 document.getElementById("ul_usuarios").innerHTML = "<li class='w3-padding-16'>" +   
                                                                         "<img src='https://www.w3schools.com/w3images/avatar6.png' class='w3-left w3-circle w3-margin-right' style='width:35px'>" +
