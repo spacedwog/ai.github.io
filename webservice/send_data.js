fb681d2e8ec20eecf85be18ec9a5909a104845cb
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-03 17:29:02 +0000

    Commited

--- webservice/send_data.js
+++ webservice/send_data.js
@@ -9,19 +9,6 @@ async function usuario() {
             alert("Novo Usuário(a): " + getIpAddress());
             ipAddress += "<p id='" + ipAddress + "'>" + "</p>";
             console.log(data.ip);
-            const xmlhttp = new XMLHttpRequest();
-            xmlhttp.onload = function() {
-                const myObj = JSON.parse(getIpAddress());
-                const myJson = JSON.stringify(myObj);
-                document.getElementById("ul_usuarios").innerHTML = "</br><li class='w3-padding-16'>" +   
-                                                                        "<img src='https://www.w3schools.com/w3images/avatar6.png' class='w3-left w3-circle w3-margin-right' style='width:35px'>" +
-                                                                        "<span class='w3-xlarge'>" + getIpAddress() + "</span><br></br>"
-                                                                    "</li>";
-            };
-            xmlhttp.open("GET", "usuario.txt");
-            alert("Abrindo arquivo");
-            xmlhttp.send();
-            alert("Dados Enviados");
         })
         .catch(error => {
             console.log('Error:', error);
